811635b5fedba13863836d98e8b79631fd21c364
Liubomyr Byelyayev <skywalker.lyub@gmail.com>
2025-09-06 12:36:24 +0300

    refactoring and further development

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import requests
+import json
 
 def main():
-    url = "https://vilni-zl.edus.school/api/user/login/"
+
+    class User:
+        def __init__(self, uuid, access, refresh):
+            self.uuid: str = uuid
+            self.access: str = access
+            self.refresh: str = refresh
+    
+    url = "https://vilni-zl.edus.school/api"
 
     login_data = {
         "login": "Любомир",
@@ -11,11 +19,21 @@ def main():
         'upload_field_name': ('my_dummy_file.txt', b'Dummy content.', 'text/plain')
     }
     
-    response = requests.post(url, data=login_data, files=dummy_file)
+    res_auth = requests.post(url + "/user/login/", data=login_data, files=dummy_file)
 
-    print(f"Status Code: {response.status_code}")
-    print("Response JSON:")
-    print(response.json())
+    print(f"Status Code: {res_auth.status_code}")
+    auth_json = res_auth.json()
+
+    user = User(auth_json["uuid"], auth_json["access"], auth_json["refresh"])
+    header_auth = { "Authorization": f'Bearer {user.access}' }
+    print(f'access = {user.access}')
+    print(f'refresh = {user.refresh}')
+    print(f'uuid = {user.uuid}')
+
+    res_homework = requests.get(url + f'/student/homework-tasks/{user.uuid}/active/', headers=header_auth)
+    homework_json = res_homework.json()
+
+    print(homework_json)
 
 if __name__ == "__main__":
     main()
